ec283f32470a3f93c5e691e3ffb761c8fc24be5a
Nick Reed <reednj77@gmail.com>
2012-06-04 02:15:35 -0500

    moved coffeescript gem outside assets group

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,7 @@
 class PagesController < ApplicationController
   def home
     @page = %w[about contact education experience portfolio skills].include?(params[:page]) ? params[:page] : 'home'
-    respond_to do |format|
-      format.html { render @page }
-      format.js { render @page }
-    end
+    render @page
   end
   
   def contact
