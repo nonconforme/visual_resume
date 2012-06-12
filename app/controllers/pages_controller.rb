3e517d45b7a9161c663a08514942faeadd7bbac5
Nick Reed <reednj77@gmail.com>
2012-06-12 14:47:33 -0500

    redirect to root if invalid page requested

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,20 @@
 class PagesController < ApplicationController
+  before_filter :validate_page, :only => :home
+  
   def home
-    @page = %w[about contact education experience portfolio skills].include?(params[:page]) ? params[:page] : 'home'
     render @page
   end
   
   def contact
     AdminMailer.contact(params['contact']).deliver
     render 'contact_submission'
+  end
+  
+  private
+  
+  def validate_page
+    is_page = %w[about contact education experience portfolio skills].include?(params[:page])
+    redirect_to :root if params[:page] && !is_page
+    @page = is_page ? params[:page] : 'home'
   end
 end
