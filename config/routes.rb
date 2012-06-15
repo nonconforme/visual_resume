0048c8fd8e4773c88f4044154e70bca7ce1273ce
Nick Reed <reednj77@gmail.com>
2012-06-14 19:22:42 -0500

    Rack response for New Relic monitor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 VisualResume::Application.routes.draw do
   post 'contact', :to => 'pages#contact'
   match '/:page', :to => 'pages#home', :as => :page
+  root :to => proc {|env| [200, {}, ["I'm alive"]] }, :constraints => { :user_agent => /NewRelicPinger/ }
   root :to => "pages#home"
 end
