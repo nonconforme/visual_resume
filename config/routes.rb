2676eaf5ba093cda68a129cd8e90fd8ba4270a14
Nick Reed <reednj77@gmail.com>
2012-06-14 20:12:55 -0500

    removed rack response for New Relic

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 VisualResume::Application.routes.draw do
   post 'contact', :to => 'pages#contact'
   match '/:page', :to => 'pages#home', :as => :page
-  root :to => proc {|env| [200, {}, ["I'm alive"]] }, :constraints => { :user_agent => /NewRelicPinger/ }
   root :to => "pages#home"
 end
